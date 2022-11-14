e851581a031ae4ce9515ae027402d380213c4aaf
r4sinhas <rasinhas@ua.pt>
2022-11-14 00:47:51 +0000

    PSIT-24 created Spring project

---
+++ backend/PASSITService/src/main/java/com/PASSIT/PASSITService/PassitServiceApplication.java
@@ -0,0 +1,13 @@
+package com.PASSIT.PASSITService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassitServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassitServiceApplication.class, args);
+	}
+
+}
---
+++ backend/PASSITService/src/test/java/com/PASSIT/PASSITService/PassitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PASSIT.PASSITService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassitServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
