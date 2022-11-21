591dc5ca3eb4c28b7e48faeefa5997cb3a864263
dferrero17 <danielmartinsferreira@ua.pt>
2022-11-21 02:38:51 +0000

    added navbar and initial hero

--- frontend/PASSIT/src/App.jsx
+++ frontend/PASSIT/src/App.jsx
@@ -1,55 +1,68 @@
-import { useState } from 'react'
-import './App.css'
-import {
-
-  BrowserRouter as Router,
-
-  Routes,
-
-  Route
-
-} from "react-router-dom";
-
-function App() {
-
-  var visible = true;
+import { useState } from "react";
+const App = () => {
+  const [open, setOpen] = useState(true);
+  const Menus = [
+    { title: "Dashboard", src: "Chart_fill" },
+    { title: "Inbox", src: "Chat" },
+    { title: "Accounts", src: "User", gap: true },
+    { title: "Schedule ", src: "Calendar" },
+    { title: "Search", src: "Search" },
+    { title: "Analytics", src: "Chart" },
+    { title: "Files ", src: "Folder", gap: true },
+    { title: "Settings", src: "Setting" },
+  ];
 
   return (
-    <div className="App">
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content text-center">
-          <div className="max-w-md">
-            <div className="card bg-base-100">
-              <div className="stack">
-                <div className="card shadow-md bg-primary text-primary-content">
-                  <div className="card-body">
-                    <h2 className="card-title">Putas</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-                  </div>
-                </div>
-                <div className="card shadow bg-primary text-primary-content {this.state.visible?'fadeIn':'fadeOut'}">
-                  <div className="card-body">
-                    <h2 className="card-title">Notification 2</h2>
-                    <p>You have 3 unread messages. Tap here to see.</p>
-                  </div>
-                </div>
-                <div className="card shadow-sm bg-primary text-primary-content">
-                  <div className="card-body">
-                    <h2 className="card-title">Notification 3</h2>
-                    <p>You have 3 unread messages. Tap here to see.</p>
-                  </div>
-                </div>
-              </div>
-              <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-              <button className="btn btn-primary animate-pulse " onClick={() => {document.getElementsByClassName("card shadow-md bg-primary text-primary-content")[0].classList.add("invisible")}}>Get Started</button>
+    <div className="flex">
+      <div
+        className={` ${open ? "w-72" : "w-20 "
+          } bg-green-700 h-screen p-5  pt-8 relative duration-300`}
+      >
+        <img
+          src="./src/assets/control.png"
+          className={`absolute cursor-pointer -right-3 top-9 w-7 border-dark-purple
+           border-2 rounded-full  ${!open && "rotate-180"}`}
+          onClick={() => setOpen(!open)}
+        />
+        <div className="flex gap-x-4 items-center ">
+          <img
+            src="./src/assets/logo-no-background.png"
+            className={`w-10 cursor-pointer duration-500 ${open && "rotate-[360deg]"
+              }`}
+          />
+          <h1
+            className={`text-white origin-left font-medium text-xl duration-200 ${!open && "scale-0"
+              }`}
+          >
+          </h1>
+        </div>
+        <ul className="pt-6">
+          {Menus.map((Menu, index) => (
+            <li
+              key={index}
+              className={`flex rounded-md p-2 cursor-pointer transition ease-in-out delay-150 bg-green-700 hover:-translate-y-1 hover:scale-110 hover:bg-green-900 duration-300 text-gray-300 text-sm items-center gap-x-4 
+              ${Menu.gap ? "mt-9" : "mt-2"} ${index === 0 && "bg-light-white"
+                } `}
+            >
+              <img src={`./src/assets/${Menu.src}.png`} />
+              <span className={`${!open && "hidden"} origin-left duration-200`}>
+                {Menu.title}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+        <div className="hero min-h-screen" style={{ backgroundImage: `url("https://www.sopitas.com/wp-content/uploads/2022/11/Cristiano-Ronaldo-y-Lionel-Messi-para-Louis-Vuitton.jpg?w=1200")` }}>
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-content text-center text-neutral-content">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold">como é que se joga isto?</h1>
+              <p className="mb-5">sei lá, sou lindo siuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu</p>
+              <button className="btn btn-primary">Get Started</button>
             </div>
           </div>
         </div>
-      </div>
     </div>
-      )
-}
-
-
-
-      export default App
+  );
+};
+export default App;
