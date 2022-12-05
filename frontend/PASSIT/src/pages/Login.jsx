fd85f819b1135a3959053777b17e5a76df6d3a4e
dferrero17 <danielmartinsferreira@ua.pt>
2022-12-05 01:51:29 +0000

    implemented inital login page

--- frontend/PASSIT/src/pages/Login.jsx
+++ frontend/PASSIT/src/pages/Login.jsx
@@ -1,9 +1,47 @@
+import React from 'react'
+
+import "./Login.css";
+
+import { useNavigate } from "react-router-dom";
 
 
-export const Login = () => {
+export function Login() {
+  const navigate = useNavigate();
+
+  const coursesPage = () => {
+    navigate("./Landing");
+  }
   return (
     <div className="flex ">
-      PUTAS
+      <div className="font-3xl absolute left-12 top-12 cursor-pointer" onClick={coursesPage}><img
+        src="./src/assets/logo-no-background.png"
+        className={`w-12`}
+      /></div>
+      <div className="hero min-h-screen" style={{ backgroundImage: `url("https://img.freepik.com/premium-photo/soccer-field-center-ball-top-view-background-sport-athletic-concept-3d-illustration-rendering_10307-2012.jpg?w=2000")` }}>
+        <div className="big-card card m-auto bg-secondary-content text-primary-content bg-opacity-90">
+
+          <div className="login-box mt-28">
+            <h2 className='text-3xl'>L O G I N</h2>
+            <form type='submit' method='POST'>
+              <div class="relative z-0 pb-5">
+                <input type="text" id="floating_standard" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-400 dark:focus:border-primary focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                <label for="floating_standard" class="absolute text-sm text-white dark:text-white duration-300 transform -translate-y-6 scale-75 top-3 right-32 -z-10 origin-[1] peer-focus:right-32  peer-focus:text-primary peer-focus:dark:text-primary-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Username</label>
+              </div>
+              <div class="relative z-0">
+                <input type="password" id="floating_standard" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-400 dark:focus:border-primary focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                <label for="floating_standard" class="absolute text-sm text-white dark:text-white duration-300 transform -translate-y-6 scale-75 top-3 right-32 -z-10 origin-[1] peer-focus:right-32 peer-focus:text-primary peer-focus:dark:text-primary-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
+              </div>
+              <a href="#">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Submit
+              </a>
+            </form>
+          </div>
+        </div>
+      </div>
     </div >
   );
 };
