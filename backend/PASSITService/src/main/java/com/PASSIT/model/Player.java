af9dce6e1d2302eb734215576ce71737111f7dc1
GoncaloSilva25 <goncassilva02@gmail.com>
2022-12-14 20:44:09 +0000

    change in player model

--- backend/PASSITService/src/main/java/com/PASSIT/model/Player.java
+++ backend/PASSITService/src/main/java/com/PASSIT/model/Player.java
@@ -1,8 +1,19 @@
 package com.PASSIT.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.MapKeyColumn;
+import javax.persistence.MapKeyJoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -19,6 +30,16 @@ public class Player extends User {
     private int weight;
     private int number;
 
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "statsByGame", joinColumns = @JoinColumn(name = "player_id"), inverseJoinColumns = @JoinColumn(name = "stats_id"))
+    @MapKeyJoinColumn(name = "seller_id")
+    private Map<Integer, ArrayList<TreeMap<String, ArrayList<TreeMap<Double, Double>>>>> stats = new HashMap<>();
+    // jogoID ~ish (just a number to sort the games)
+    // 1 : { ["heartRate" : [ { 0.0012 : 90 }, { 0.23 : 76 }, { 1.230 : 104 } ] }],
+    // "o2 levels2 : [{ 0.01231 : 123123}]]},
+    // 2 : { ["heartRate" : [ { 0.0 : 0.0 }, { 1.0 : 1.0 }, { 2.0 : 2.0 }], "o2
+    // levels2 : [{ 0.01231 : 123123}], ...
+
     public Player() {
         super();
     }
@@ -33,7 +54,6 @@ public class Player extends User {
         this.weight = weight;
         this.number = number;
     }
-
 
     public String getName() {
         return super.getName();
